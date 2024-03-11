1f31ae77aa5614d998b64d7e26ffc22ceb4f191b
AlekseyAleksandrov <axcerz@gmail.com>
2024-03-11 21:19:53 +0300

    Создали базовые стили приложения, Создали подключение к API валют

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import './App.css'
 
-function App() {
+const getValutes = async () => {
+	const res = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+ 
+	if (!res.ok) {
+	  throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+	}
+ 
+	return await res.json()
+ }
+ 
+ const valutes = getValutes();
+ 
+ const USD = valutes.then(res => res.Valute['USD']);
+ const EUR = valutes.then(res => res.Valute['EUR']);
+
+const App = (props) => {
+  const [valute, setValute] = useState('RUB');
+  const [value, setValue] = useState(props.value);
+  
+  useEffect(() => {
+    
+  }, [valute])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="counter">{ value }</div>
+      <div className="controls">
+        <button onClick={ () => setValute('RUB') }>RUB</button>
+        <button onClick={ () => setValute('USD') }>USD</button>
+        <button onClick={ () => setValute('EUR') }>EUR</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
